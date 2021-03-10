2598d6f5b5e0f1672347985ec3b496ac39a75110
Tony Ciccarone <tonyvciccarone@gmail.com>
2021-03-10 18:53:14 -0500

    basic child theme init

--- functions.php
+++ functions.php
@@ -2,5 +2,10 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'ciccarone-style', get_stylesheet_directory_uri() . '/style.css' );
 }
-?>
+
+function company_name() {
+  return 'Rise Capital Funding';
+}
+add_shortcode('company_name', 'company_name');
